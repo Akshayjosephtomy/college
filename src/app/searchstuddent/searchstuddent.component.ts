48a4a8457cf13a10117742e9dc61e237d7fcc961
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-08 17:00:14 +0530

    search

---
+++ src/app/searchstuddent/searchstuddent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchstuddent',
+  templateUrl: './searchstuddent.component.html',
+  styleUrls: ['./searchstuddent.component.css']
+})
+export class SearchstuddentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
